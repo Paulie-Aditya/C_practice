52cb6c416774f385b1e67906d813c2fd5c4e7bc4
Paulie-Aditya <122125787+Paulie-Aditya@users.noreply.github.com>
2023-05-10 23:29:22 +0000

    Thu, May 11, 2023, 4:59 AM +05:30

--- fibo.c
+++ fibo.c
@@ -2,4 +2,29 @@
 
 int main(void){
     int a = 0, b = 1;
+
+    int n;
+    printf("Enter n to know the nth Fibonacci Number: ");
+    scanf("%d",&n);
+
+    if(n<0){
+        printf("Not valid");
+        return 0;
+    }
+    else if (n == 0){
+        printf("0");
+        return 0 ;
+    }
+    else if(n == 1){
+        printf("1");
+        return 0;
+    }
+    int result;
+
+    for(int i = 1;i<=n;i++){
+        result = a+b;
+        a = b;
+        b = result;
+    }
+    printf("%d",result);
 }
