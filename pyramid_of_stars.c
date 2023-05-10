5d9304862ac5f87cbe5a95cad3cc63307e545c16
Paulie-Aditya <122125787+Paulie-Aditya@users.noreply.github.com>
2023-05-10 21:16:49 +0000

    Thu, May 11, 2023, 2:46 AM +05:30

--- pyramid_of_stars.c
+++ pyramid_of_stars.c
@@ -6,11 +6,11 @@ int main(void)
     int n = scanf("%d",&n);
 
     int i,j;
-    for(i=0;i<n;i++)
+    for(i=1;i<=n;i++)
     {
-        for(j = 1;j<(2*n -1);j++)
+        for(j = 1;j<2*n -1;j++)
         {
-            if(j>=(n-(i-1)) && j<=(n+(i-1)))
+            if(j >= n-(i-1) && j <= n+(i-1))
             {
                 printf("*");
             }
