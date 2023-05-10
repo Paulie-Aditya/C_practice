066d286163ca0a642050c9d0a29330662f9ad40a
Paulie-Aditya <122125787+Paulie-Aditya@users.noreply.github.com>
2023-05-10 22:59:01 +0000

    Thu, May 11, 2023, 4:29 AM +05:30

--- prime.c
+++ prime.c
@@ -1,2 +1,26 @@
 #include <stdio.h>
 #include <math.h>
+
+int main(void){
+    printf("Enter Number: \n");
+    int n ;
+    scanf("%d",&n);
+
+    if (n <=1){
+        printf("False");
+        return 0;
+    }
+    else if (n ==2){
+        printf("True");
+        return 0;
+    }
+
+    for(int i = 2;i<=ceil(sqrt(n));i++){
+        if (n%i){
+            printf("False");
+            return 0;
+        }
+    }
+    printf("True");
+    return 0;
+}
