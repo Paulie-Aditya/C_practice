2db2d34bf43dc324734946c19dd4c22e1acca194
Paulie-Aditya <122125787+Paulie-Aditya@users.noreply.github.com>
2023-05-10 02:08:14 +0000

    Wed, May 10, 2023, 7:38 AM +05:30

--- practice.c
+++ practice.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
+/*
 #define add(x,y) x+y
 
 int main(){
     printf("%d \n",5*add(4,3));
     return 0;
 }
+*/
+
+int main(){
+    int a =4,b=3;
+    printf("%d",a+++++b);
+    return 0;
+}
+
