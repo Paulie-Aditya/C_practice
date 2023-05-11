9801a9cef77227cd0180efd2f24190efeca12747
Paulie-Aditya <122125787+Paulie-Aditya@users.noreply.github.com>
2023-05-11 14:11:21 +0530

    floyd

--- floyd.c
+++ floyd.c
@@ -6,12 +6,17 @@
 
 int main(void){
     printf("Enter number of rows you want : \n");
-    int n ;
-    scanf("%d",&n);
-    int j = 1;
-    for(int i = 1;i<=n;i++){
-        for(;j<=i;j++){
-            printf("%d",j);
+    int rows ;
+    scanf("%d",&rows);
+
+    int n=1;
+
+    for(int i = 1;i<=rows;i++)
+    {
+        for(int j = 1;j<=i;j++)
+        {
+            printf("%d ",n);
+            n++;
         }
         printf("\n");
     }
