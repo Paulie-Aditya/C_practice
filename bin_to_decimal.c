9e80ee926c80f3739e635c32565faf464dcae8ed
Paulie-Aditya <122125787+Paulie-Aditya@users.noreply.github.com>
2023-05-11 14:26:56 +0530

    Binary to Decimal

--- bin_to_decimal.c
+++ bin_to_decimal.c
@@ -1,11 +1,21 @@
 //Binary to Decimal
 
 #include <stdio.h>
+#include <math.h>
 
 int main(void){
     printf("Enter Binary Number: \n");
     int n;
     scanf("%d",&n);
 
-    
+    int result =0, q = n, power = 0,rem;
+
+    while (q!=0){
+        rem = q%10;
+        result += rem*(pow(2,power));
+        power++;
+        q = q/10;
+    }
+    printf("Integer Representation: %d",result);
+    return 0;
 }
