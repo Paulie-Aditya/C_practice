ea4c7fd552d3f20d84cb217e9b49dbd14de16737
Paulie-Aditya <122125787+Paulie-Aditya@users.noreply.github.com>
2023-05-11 09:20:19 +0000

    Thu, May 11, 2023, 2:50 PM +05:30

---
+++ leap_year.c
@@ -0,0 +1,30 @@
+//Check whether a year is a leap year or not
+#include <stdio.h>
+
+int main(void){
+    int year;
+    printf("Enter Year: ");
+    scanf("%d",&year);
+
+    if (year<0){
+        printf("Invalid year");
+        return 0;
+    }
+
+    if ((year%4 == 0)){
+        if (year%100 == 0 && year%400 == 0){
+            printf("Leap Year");
+        }
+        else if(year%100 == 0){
+            printf("Not a Leap Year");
+        }
+
+        else{
+            printf("Leap Year");
+        }
+    }
+    else{
+        printf("Not a leap year");
+    }
+    return 0;
+}
