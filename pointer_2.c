b32e59b755edc5931dfd1b6b744bfe5280de9bc9
Paulie-Aditya <122125787+Paulie-Aditya@users.noreply.github.com>
2023-05-12 21:28:15 +0000

    Sat, May 13, 2023, 2:58 AM +05:30

---
+++ pointer_2.c
@@ -0,0 +1,14 @@
+//Finding mid of array using pointers
+#include <stdio.h>
+
+int *findMid(int a[],int n){
+    return &a[n/2];
+}
+
+int main(){
+    int a[] = {1,2,3,4,5};
+    int n = sizeof(a)/sizeof(a[0]);
+    int *mid = findMid(a,n);
+    printf("%d \n",*mid);
+    return 0;
+}
