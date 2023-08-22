a1eb068e26d91159d03cfa962a081f25efff78f3
Paulie-Aditya <paul.aditya304@gmail.com>
2023-08-22 06:40:55 +0530

    insertion at some pos is still dicey

--- LL_basic.c
+++ LL_basic.c
@@ -4,13 +4,14 @@
 struct ListNode{
     int data;
     struct ListNode *next;
-};
+}l1;
 void print(struct ListNode *head){
     struct ListNode *curr = head;
     while(curr != NULL){
         printf("%d ->",curr->data);
         curr = curr ->next;
-    };
+    }
+    printf("NULL");
 
 }
 
@@ -63,6 +64,40 @@ struct ListNode* insertAtEnd(struct ListNode* head, int data){
 
     return head;
 }
+struct ListNode* insertAtpos(struct ListNode* head, int data, int pos){
+    struct ListNode* newNode;
+
+    newNode = (struct ListNode*)malloc(sizeof(struct ListNode));
+    newNode->data = data;
+    newNode->next = NULL;
+
+    int count = 0;
+    if(head == NULL){
+        head = newNode;
+        head->next = NULL;
+    }
+    struct ListNode* curr = head;
+    while(curr != NULL){
+        if(count == pos-1){
+            break;
+        }
+        curr = curr->next;
+    }
+    newNode->next = curr->next;
+    curr->next = newNode;
+
+    return head;
+
+}
 int main(void){
+    struct ListNode* head = &l1;
+    insertAtEnd(head,1);
+    insertAtEnd(head,2);
+    insertAtEnd(head,4);
+    insertAtEnd(head,5);
+
+    print(head);
+    //insertAtpos(head, 3,3);
+    print(head);
     return 0;
 }
